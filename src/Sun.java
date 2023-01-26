742aa4837c19316142c63ed24334e3bce9eee114
mv-stns <21103251+mv-stns@users.noreply.github.com>
2023-01-26 21:35:48 +0100

    changed visuals of sun & moon

--- src/Sun.java
+++ src/Sun.java
@@ -15,7 +15,18 @@ public class Sun {
     public void draw(Graphics g){
             Graphics2D g2d = (Graphics2D) g;
         if (dayTime) {
+            g2d.setColor(new Color(255, 255, 255, 100));
+            g2d.fillOval(x - 10, y - 10, radius*2 + 20, radius*2 + 20);
+            g2d.setColor(new Color(255, 255, 255, 50));
+            g2d.fillOval(x - 20, y - 20, radius*2 + 40, radius*2 + 40);
+            g2d.setColor(new Color(255, 255, 255, 25));
+            g2d.fillOval(x - 30, y - 30, radius*2 + 60, radius*2 + 60);
+
             g2d.setColor(Color.YELLOW);
+            g2d.fillOval(x,y,radius*2,radius*2);
+            // draw radial gradient around sun.
+            RadialGradientPaint p = new RadialGradientPaint(x + radius, y + radius, radius, new float[]{0.0f, 1.0f}, new Color[]{new Color(255, 255, 255, 0), new Color(255, 235, 147, 192)});
+            g2d.setPaint(p);
             g2d.fillOval(x,y,radius*2,radius*2);
         } else {
             g2d.setColor(new Color(103, 103, 103, 100));
@@ -23,7 +34,11 @@ public class Sun {
             g2d.setColor(new Color(114, 114, 114, 100));
             g2d.setStroke(new BasicStroke(4));
             g2d.drawOval(x,y,radius*2,radius*2);
+            RadialGradientPaint p = new RadialGradientPaint(x + radius, y + radius, radius, new float[]{0.0f, 1.0f}, new Color[]{new Color(255, 255, 255, 0), new Color(255, 255, 255, 100)});
+            g2d.setPaint(p);
+            g2d.fillOval(x,y,radius*2,radius*2);
         }
+
     }
 
     public void switchTime() {
